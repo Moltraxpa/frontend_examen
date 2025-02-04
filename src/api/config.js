896627bd54429dab121c28ef3ef08699e297f354
Milton Flores <mfloresm10@est.ups.edu.ec>
2025-02-04 13:23:19 -0500

    commit

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 
       //'http://localhost:3030/api',
-     'https://examen-backend-ruby.vercel.app/api',
+     'https://backend-examen-44h8.vercel.app/',
     headers: {
         'Content-Type': 'application/json'
     }
